3ac1461f8553354298ffa28624f320916d414704
JOSHIN SAJI <152678515+joshinsaji@users.noreply.github.com>
2025-11-08 15:13:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'your-repo-name' to your actual GitHub repo name if deploying to GitHub Pages
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: "/Personal-portfolio/"
 })
